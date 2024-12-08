d05a45591d559dd3e0c790d0ba11445f7cd90e0c
Michael Tesař <michtesar@gmail.com>
2024-12-08 16:40:58 +0100

    Remove commented-out puzzle grid from main function
    The redundant commented-out puzzle grid has been removed for code clarity. This change ensures that the focus remains on the functional aspects of the main function without unnecessary distractions. The active code utilizes `load_puzzle_grid()`, making the hard-coded grid obsolete.

--- src/bin/day_04.rs
+++ src/bin/day_04.rs
@@ -140,18 +140,6 @@ fn load_puzzle_grid() -> Vec<Vec<char>> {
 
 fn main() {
     let puzzle_grid = load_puzzle_grid();
-    // let puzzle_grid = [
-    //     ["M", "M", "M", "S", "X", "X", "M", "A", "S", "M"],
-    //     ["M", "S", "A", "M", "X", "M", "S", "M", "S", "A"],
-    //     ["A", "M", "X", "S", "X", "M", "A", "A", "M", "M"],
-    //     ["M", "S", "A", "M", "A", "S", "M", "S", "M", "X"],
-    //     ["X", "M", "A", "S", "A", "M", "X", "A", "M", "M"],
-    //     ["X", "X", "A", "M", "M", "X", "X", "A", "M", "A"],
-    //     ["S", "M", "S", "M", "S", "A", "S", "X", "S", "S"],
-    //     ["S", "A", "X", "A", "M", "A", "S", "A", "A", "A"],
-    //     ["M", "A", "M", "M", "M", "X", "M", "M", "M", "M"],
-    //     ["M", "X", "M", "X", "A", "X", "M", "A", "S", "X"],
-    // ];
     let search_word = "XMAS".to_lowercase();
     let search_word_length = search_word.len();
 
